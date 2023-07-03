8b8d1dcefe743886a130e9be5f8879d265f4d9dd
alexeynau <firnen76@gmail.com>
2023-07-03 22:15:00 +0300

    started 4 chapter

---
+++ 4_understanding_ownership/what_is_ownership/target/debug/deps/what_is_ownership-b06edc6360b07803.d
@@ -0,0 +1,5 @@
+D:\Projects\Rust\rust-course\4_understanding_ownership\what_is_ownership\target\debug\deps\what_is_ownership-b06edc6360b07803.rmeta: src\main.rs
+
+D:\Projects\Rust\rust-course\4_understanding_ownership\what_is_ownership\target\debug\deps\what_is_ownership-b06edc6360b07803.d: src\main.rs
+
+src\main.rs:
