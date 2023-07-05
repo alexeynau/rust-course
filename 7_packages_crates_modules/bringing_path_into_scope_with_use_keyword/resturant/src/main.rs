47ff876ab10a5f6866de0cb3b766a5a04015d218
alexeynau <firnen76@gmail.com>
2023-07-05 20:32:18 +0300

    add others tasks

---
+++ 7_packages_crates_modules/bringing_path_into_scope_with_use_keyword/resturant/src/main.rs
@@ -0,0 +1,9 @@
+use std::collections::*;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+}
