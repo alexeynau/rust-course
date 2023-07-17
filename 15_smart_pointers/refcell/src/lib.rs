550915531db7f049013e6e2f4a4287778b514561
lechatnau <firnen76@gmail.com>
2023-07-17 17:15:39 +0300

    add refcell

---
+++ 15_smart_pointers/refcell/src/lib.rs
@@ -0,0 +1,81 @@
+//A library to keep track of how close a value is to a maximum value and warn when the value is at certain levels
+
+//declare a new trait
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+// declare new struct
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+//create method for our struct that works if in params we get a Message trait
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    // builder
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+    //method that set value and compare it with max value
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    //store the sent_messages within a RefCell<T>, and then the send method will...
+    //... be able to modify sent_messages to store the messages we’ve seen
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        //method to create new entity of MockMessenger struct
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        //method where we use RefCell
+        fn send(&self, message: &str) {
+            //we borrow mutable ref and change vector
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
