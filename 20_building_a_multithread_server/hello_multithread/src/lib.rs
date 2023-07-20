c86caca4cfec97928ade520a5884fc5b723dbed1
lechatnau <firnen76@gmail.com>
2023-07-20 18:15:06 +0300

    add comments

--- 20_building_a_multithread_server/hello_multithread/src/lib.rs
+++ 20_building_a_multithread_server/hello_multithread/src/lib.rs
@@ -2,12 +2,12 @@ use std::{
     sync::{mpsc, Arc, Mutex},
     thread,
 };
-
+// that struct give jobs its thread, manage threads
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
 }
-
+// type name of job that thread do. Should be a clousure implements Send trait
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
@@ -20,47 +20,51 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-
+        // get channel reciver and sender
         let (sender, receiver) = mpsc::channel();
-
+        // to share ownership across multiple threads and allow the threads to mutate the value
+        // we need to use Arc<Mutex<T>>
         let receiver = Arc::new(Mutex::new(receiver));
-
+        // init workers with size given in params
         let mut workers = Vec::with_capacity(size);
-
+        // push new worker in workers with clonned reciever
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-
+        // return new thread pool
         ThreadPool { workers, sender }
     }
-
+    /// Take a job given in params and send it to thread
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send + 'static,
+        F: FnOnce() + Send + 'static, // takes clousure
     {
+        // create a job from clousure
         let job = Box::new(f);
-
+        // send a job through a sender
         self.sender.send(job).unwrap();
     }
 }
-
+/// Take a job and do it
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
 
-// --snip--
 
 impl Worker {
+    /// Create a new `Worker` with id and reciver given in params
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
+        /// create a thread
+        let thread = thread::spawn(move || loop { // infinite loop
+            // wait for getting a new job
             let job = receiver.lock().unwrap().recv().unwrap();
 
             println!("Worker {id} got a job; executing.");
-
+            // do job
             job();
         });
-
+        // return worker
         Worker { id, thread }
     }
 }
--- 20_building_a_multithread_server/hello_multithread/src/main.rs
+++ 20_building_a_multithread_server/hello_multithread/src/main.rs
@@ -8,35 +8,45 @@ use std::{
 };
 
 fn main() {
+    // create TCP Listener for connection
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // create thread pool with size 4
     let pool = ThreadPool::new(4);
-
+    // listen request in stream
     for stream in listener.incoming() {
+        // get a stream
         let stream = stream.unwrap();
-
+        // give job to thread pool
         pool.execute(|| {
             handle_connection(stream);
         });
     }
 }
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
+fn handle_connection(mut stream: TcpStream) {
+    // read a request from stream
+    let buf_reader = BufReader::new(&mut stream);
+    // get a request from buffer
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // return page according to request
     let (status_line, filename) = match &request_line[..] {
+        // successz request
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
+            // example of long request
             thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "hello.html")
         }
+        // fail request
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
-
+    // read html page
     let contents = fs::read_to_string(filename).unwrap();
+    // get content length
     let length = contents.len();
-
+    // create a response
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
+    // send response
     stream.write_all(response.as_bytes()).unwrap();
 }
